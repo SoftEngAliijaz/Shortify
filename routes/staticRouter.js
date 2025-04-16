30e37505a1f094dc892b352600ddf79783909b1b
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-04-17 02:02:43 +0500

    enhance home page with dynamic URL rendering and improved styling

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const URL = require("../models/url_model");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.render("home");
+router.get("/", async (req, res) => {
+  try {
+    const allUrls = await URL.find({});
+    res.render("home", { urls: allUrls });
+  } catch (err) {
+    console.error("Error fetching URLs:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
